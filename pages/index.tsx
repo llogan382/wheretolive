841518d8f177a16356a80ec31eb05205ab9f4258
Luke <llogan382@gmail.com>
2021-09-10 10:38:15 -0400

    fixes an error

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,16 +26,10 @@ const Home: NextPage = () => {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Powered by{' Logan Web Dev'}
           <span className={styles.logo}>
 
           </span>
-        </a>
       </footer>
     </div>
   )
