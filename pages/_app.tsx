43359bf1807f363f4b3c50d1bd3e5faf4627716f
Luke <llogan382@gmail.com>
2021-09-21 05:31:00 -0400

    chore: updates todos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import FormProvider from '../context'
 import '../styles/globals.css'
-
+// TODO: Update TSCONFIG
+// TODO: Update CI
+// TODO: Write Tests
 function MyApp({ Component, pageProps }) {
   return (
     <FormProvider>
